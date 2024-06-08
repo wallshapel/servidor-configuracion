3041836adf937f88a4a0815010871b8864386f74
legato <abelyimartinez1987@hotmail.com>
2024-06-08 07:32:07 -0500

    Primer commit

---
+++ user/src/main/java/com/user_service/user/services/UserService.java
@@ -0,0 +1,106 @@
+package com.user_service.user.services;
+
+import com.user_service.user.entities.User;
+import com.user_service.user.feignclients.CarFeignClient;
+import com.user_service.user.feignclients.MotoFeignClient;
+import com.user_service.user.models.Car;
+import com.user_service.user.models.Moto;
+import com.user_service.user.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private RestTemplate restTemplate; // Necesario para comunicarse con otros microservicios
+
+    // Método para devolver los carros de un usuario por su id
+    public List<Car> getCars(Long userId) {
+        String url = "http://localhost:8081/api/v1/cars/user/" + userId;
+        ResponseEntity<List<Car>> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Car>>() {}
+        );
+        return response.getBody();
+    }
+
+    // Método para devolver las motos de un usuario por su id
+    public List<Moto> getMotos(Long userId) {
+        String url = "http://localhost:8082/api/v1/motos/user/" + userId;
+        ResponseEntity<List<Moto>> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Moto>>() {}
+        );
+        return response.getBody();
+    }
+
+    @Autowired
+    private CarFeignClient carFeignClient;
+
+    public Car storeCar(Long userId, Car car) {
+        car.setUserId(userId);
+        return carFeignClient.store(car);
+    }
+
+    @Autowired
+    private MotoFeignClient motoFeignClient;
+
+    public Moto storeMoto(Long userId, Moto moto) {
+        moto.setUserId(userId);
+        return motoFeignClient.store(moto);
+    }
+
+    public Map<String, Object> getUserAndVehicles(Long userId) {
+        Map<String, Object> result = new HashMap<>();
+        User user = userRepository.findById(userId).orElse(null);
+        if (user == null) {
+            result.put("Message", "User not found");
+            return result;
+        }
+        result.put("User", user);
+
+        List<Car> cars = carFeignClient.getCars(userId);
+        if (cars.isEmpty())
+            result.put("Cars", "The user has not cars");
+        else
+            result.put("Cars", cars);
+
+        List<Moto> motos = motoFeignClient.getMotos(userId);
+        if (motos.isEmpty())
+            result.put("Motos", "The user has not motos");
+        else
+            result.put("Motos", motos);
+
+        return result;
+    }
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User>index() {
+        return userRepository.findAll();
+    }
+
+    public User show(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User store(User user) {
+        return userRepository.save(user);
+    }
+
+}
